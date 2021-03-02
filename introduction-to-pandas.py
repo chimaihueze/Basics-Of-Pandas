40ab70e63eb4cd2c9a9dab4c870eb08c70822918
Chima Ihueze <kihueze@gmail.com>
2021-03-02 20:27:19 +0100

    updated commit

--- introduction-to-pandas.py
+++ introduction-to-pandas.py
@@ -1,5 +1,8 @@
 # Pandas is a Python library used for working with data sets.
 # It has functions for analyzing, cleaning, exploring, and manipulating data.
+# Pandas allows us to analyze big data and make conclusions based on statistical theories.
+# Pandas can clean messy data sets, and make them readable and relevant.
+# Relevant data is very important in data science.
 
 import pandas as pd
 
