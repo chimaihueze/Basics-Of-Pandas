d54726244de7f84a93f0b18cc408289544688a9b
Chima Ihueze <kihueze@gmail.com>
2021-03-02 20:39:24 +0100

    updated commit

--- introduction-to-pandas.py
+++ introduction-to-pandas.py
@@ -3,8 +3,14 @@
 # Pandas allows us to analyze big data and make conclusions based on statistical theories.
 # Pandas can clean messy data sets, and make them readable and relevant.
 # Relevant data is very important in data science.
+# Pandas are also able to delete rows that are not relevant, or contains wrong values, like empty or NULL values. This is called cleaning the data.
+# In pandas, a data table is called a DataFrame.
 
 import pandas as pd
+
+
+
+
 
 
 
