72477582dc378359978075aeffbc10aa3895311b
Chima Ihueze <kihueze@gmail.com>
2021-03-03 14:28:07 +0100

    updated commit

--- pandas-dataframe.py
+++ pandas-dataframe.py
@@ -2,3 +2,8 @@
 # Series is like a column, a DataFrame is the whole table.
 
 import pandas as pd
+
+# Create a DataFrame from two Series
+
+data = {'calories': [10, 15, 20], 'duration': [20, 40, 60]}
+print(pd.DataFrame(data))
