fcef51d5086151f79dc15d020d33bb402b3a34db
Chima Ihueze <kihueze@gmail.com>
2021-03-03 14:39:07 +0100

    updated commit

--- pandas-dataframe.py
+++ pandas-dataframe.py
@@ -6,4 +6,13 @@ import pandas as pd
 # Create a DataFrame from two Series
 
 data = {'calories': [10, 15, 20], 'duration': [20, 40, 60]}
-print(pd.DataFrame(data))
+my_data =pd.DataFrame(data)
+print(my_data)
+
+# Locate Row
+# As you can see from the result above, the DataFrame is like a table with rows and columns.
+# Pandas use the loc attribute to return one or more specified row(s)
+# Return row 1:
+#refer to the row index:
+
+print(my_data.loc[1])
