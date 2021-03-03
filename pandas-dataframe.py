4c7185e49498b5ab11d9fcab15a057f6942e68d3
Chima Ihueze <kihueze@gmail.com>
2021-03-03 16:15:28 +0100

    updated commit

--- pandas-dataframe.py
+++ pandas-dataframe.py
@@ -28,4 +28,11 @@ print(my_data.loc[[0, 1]])
 # With the index argument, you can name your own indexes.
 # Add a list of names to give each row a name:
 
-print(pd.DataFrame(data, index = ['Day1', 'Day2', 'Day3']))
+my_data2 = pd.DataFrame(data, index = ['Day1', 'Day2', 'Day3'])
+print(my_data2)
+
+# Locate Named Indexes
+# Use the named index in the loc attribute to return the specified row(s).
+# Return "day2":
+
+print(my_data2.loc['Day2'])
