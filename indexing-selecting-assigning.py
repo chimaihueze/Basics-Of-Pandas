3de3d865fe2b8d6e672cddd82af9289ec69db269
Chima Ihueze <kihueze@gmail.com>
2021-03-06 17:39:59 +0100

    updated commit

--- indexing-selecting-assigning.py
+++ indexing-selecting-assigning.py
@@ -6,4 +6,13 @@ import pandas as pd
 # Native Python objects provide good ways of indexing data.
 # Pandas carries all of these over, which helps make it easy to start with.
 
+ab = (pd.read_csv("pandas_data.csv"))
+print(ab)
 
+# In Python, we can access the property of an object by accessing it as an attribute.
+# A book object, for example, might have a title property, which we can access by calling book.title.
+# Columns in a pandas DataFrame work in much the same way.
+# Hence to access the Pulse property of python_data we can use:
+
+print(ab['Pulse'])
+print(ab['Maxpulse'])
