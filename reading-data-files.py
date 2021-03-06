d981a5e0a763d0776f6996145a80266678d4ef62
Chima Ihueze <kihueze@gmail.com>
2021-03-06 14:56:43 +0100

    initial commit

---
+++ reading-data-files.py
@@ -0,0 +1,6 @@
+# Being able to create a DataFrame or Series by hand is handy.
+# But, most of the time, we won't actually be creating our own data by hand.
+# Instead, we'll be working with data that already exists.
+
+import pandas as pd  
+
