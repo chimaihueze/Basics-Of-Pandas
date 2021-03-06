7377e4ebc924018940d33661f5b1838868ed2dad
Chima Ihueze <kihueze@gmail.com>
2021-03-06 16:32:52 +0100

    initial commit

---
+++ indexing-selecting-assigning.py
@@ -0,0 +1,5 @@
+# Selecting specific values of a pandas DataFrame or Series to work on is an implicit step in almost any data operation you'll run.
+# So one of the first things you need to learn in working with data in Python is how to go about selecting the data points relevant to you quickly and effectively.
+
+import pandas as pd
+
