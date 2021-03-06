60715964face9f58e8ba60d041405198d2116ce4
Chima Ihueze <kihueze@gmail.com>
2021-03-06 14:58:21 +0100

    updated commit

--- reading-data-files.py
+++ reading-data-files.py
@@ -2,5 +2,8 @@
 # But, most of the time, we won't actually be creating our own data by hand.
 # Instead, we'll be working with data that already exists.
 
+# Data can be stored in any of a number of different forms and formats. 
+# By far the most basic of these is the humble CSV file.
+
 import pandas as pd  
 
