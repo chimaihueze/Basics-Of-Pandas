5a4c40d327065f24feff9fee97dd5016632645bb
Chima Ihueze <kihueze@gmail.com>
2021-03-11 17:51:15 +0100

    initial commit

---
+++ summary-functions-and -maps.py
@@ -0,0 +1,9 @@
+# Plucking the right data out of our data representation is critical to getting work done.
+# However, the data does not always come out of memory in the format we want it in right out of the bat.
+# Sometimes we have to do some more work ourselves to reformat it for the task at hand.
+# This tutorial will cover different operations we can apply to our data to get the input "just right".
+
+import pandas as pd
+
+abc = pd.reas_csv('pandas_data.csv')
+
