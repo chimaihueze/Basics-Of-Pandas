b659fa2662556cfbc41130ffdff10076035bcc3a
Chima Ihueze <kihueze@gmail.com>
2021-03-12 09:20:20 +0100

    updated commit

--- summary-functions-and -maps.py
+++ summary-functions-and -maps.py
@@ -46,3 +46,11 @@ print(abc.Maxpulse.value_counts())
 # A map is a term, borrowed from mathematics, for a function that takes one set of values and "maps" them to another set of values.
 # In data science we often have a need for creating new representations from existing data, or for transforming data from the format it is in now to the format that we want it to be in later. 
 # Maps are what handle this work, making them extremely important for getting your work done!
+
+# There are two mapping methods that you will use often.
+# map() is the first, and slightly simpler one.
+# For example, suppose that we wanted to remean the scores of the Pulse to 0.
+# We can do this as follows: 
+
+abcpulse_mean = abc.Pulse.mean()
+print(abc.Pulse.map(lambda p: p - abcpulse_mean))
