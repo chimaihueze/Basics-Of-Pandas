77347f59583af09037240ed72333084babd31742
Chima Ihueze <kihueze@gmail.com>
2021-03-13 10:38:50 +0100

    initial commit

---
+++ grouping-and-sorting.py
@@ -0,0 +1,7 @@
+# Maps allow us to transform data in a DataFrame or Series one value at a time for an entire column.
+# However, often we want to group our data, and then do something specific to the group the data is in.
+# As you'll learn, we do this with the groupby() operation.
+# We'll also cover some additional topics, such as more complex ways to index your DataFrames, along with how to sort your data.
+
+import pandas as pd
+
