fb86e49b3f1da2db7e9ea630968040c20d1a95a0
Chima Ihueze <kihueze@gmail.com>
2021-03-13 23:00:50 +0100

    updated commit

--- removing-duplicates.py
+++ removing-duplicates.py
@@ -2,10 +2,18 @@
 # By taking a look at our test data set, we can assume that row 11 and 12 are duplicates.
 # To discover duplicates, we can use the duplicated() method.
 # The duplicated() method returns a Boolean values for each row:
-# Example
-# Returns True for every row that is a duplicate, othwerwise False:
 
 import pandas as pd
 
 abc = pd.read_csv("dirtydata.csv")
 abc["Date"] = pd.to_datetime(abc["Date"])
+
+# Example
+# Returns True for every row that is a duplicate, othwerwise False:
+
+print(abc.duplicated())
+
+# Removing Duplicates
+# To remove duplicates, use the drop_duplicates() method.
+
+print(abc.drop_duplicates())
