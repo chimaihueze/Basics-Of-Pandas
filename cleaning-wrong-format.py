559791ccbfbf37c7d5a18b0d8c3500a64af39343
Chima Ihueze <kihueze@gmail.com>
2021-03-13 17:58:56 +0100

    updated commit

---
+++ cleaning-wrong-format.py
@@ -0,0 +1,5 @@
+# Cells with data of wrong format can make it difficult, or even impossible, to analyze data.
+# To fix it, you have two options: remove the rows, or convert all cells in the columns into the same format.
+# Convert Into a Correct Format
+# In our Data Frame, we have two cells with the wrong format.
+# Check out row 22 and 26, the 'Date' column should be a string that represents a date:
