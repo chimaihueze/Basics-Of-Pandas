7da8b6cfd831019cb87c4c09a230ab5d722bfca8
Chima Ihueze <kihueze@gmail.com>
2021-03-13 12:59:18 +0100

    updated commit

--- cleaning-data.py
+++ cleaning-data.py
@@ -10,3 +10,9 @@ import pandas as pd
 
 our_dataset = pd.read_csv("dirtydata.csv")
 print(our_dataset)
+
+# The data set contains some empty cells ("Date" in row 22, and "Calories" in row 18 and 28).
+# The data set contains wrong format ("Date" in row 26).
+# The data set contains wrong data ("Duration" in row 7).
+# The data set contains duplicates (row 11 and 12).
+
