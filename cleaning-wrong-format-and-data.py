df87aa70f4678474916448f59e1e1e0fa44f6b4b
Chima Ihueze <kihueze@gmail.com>
2021-03-13 20:34:36 +0100

    updated commit

--- cleaning-wrong-format-and-data.py
+++ cleaning-wrong-format-and-data.py
@@ -39,4 +39,15 @@ print(abc.to_string())
 abc.loc[7, 'Duration'] = 45
 print(abc.to_string)
 
+# For small data sets you might be able to replace the wrong data one by one, but not for big data sets.
+# To replace wrong data for larger data sets you can create some rules, e.g. set some boundaries for legal values, and replace any values that are outside of the boundaries.
 
+# Loop through all values in the "Duration" column.
+# If the value is higher than 120, set it to 120:
+
+abc = pd.read_csv("dirtydata.csv")
+for x in abc.index:
+    if abc.loc[x, "Duration"] > 120:
+        abc.loc[x, "Duration"] = 120
+        
+print(abc.to_string())
