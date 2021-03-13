8c941662ad8b8ba4f849a99f8d1f28c1c6fd0153
Chima Ihueze <kihueze@gmail.com>
2021-03-13 16:23:29 +0100

    initial commit

---
+++ cleaning-empty-cells.py
@@ -0,0 +1,11 @@
+# Empty cells can potentially give you a wrong result when you analyze data.
+
+# Remove Rows
+# One way to deal with empty cells is to remove rows that contain empty cells.
+# This is usually OK, since data sets can be very big, and removing a few rows will not have a big impact on the result.
+
+# Example
+# Return a new Data Frame with no empty cells:
+
+import pandas as pd
+
