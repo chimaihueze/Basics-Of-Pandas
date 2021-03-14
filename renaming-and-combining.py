8d066c7cf5e6d8783f76761a3a8caa07c404af5c
Chima Ihueze <kihueze@gmail.com>
2021-03-14 12:32:15 +0100

    updated commit

--- renaming-and-combining.py
+++ renaming-and-combining.py
@@ -7,5 +7,15 @@ import pandas as pd
 abc = pd.read_csv("pandas_data.csv")
 
 # The first function we'll introduce here is rename(), which lets you change index names and/or column names.
-# For example, to change the points column in our dataset to score, we would do:
+# For example, to change the Calories column in our dataset to Cal, we would do:
+
+db = abc.rename(columns = {"Calories": "Cal"})
+print(db)
+
+# rename() lets you rename index or column values by specifying a index or column keyword parameter, respectively.
+# It supports a variety of input formats, but usually a Python dictionary is the most convenient.
+# Here is an example using it to rename some elements of the index.
+
+
+
 
